ed57a0fe930f7441da2b2027b96828a707b60caf
Krzysztof Zawadka <kzawadka@macbook-pro.local>
2019-01-04 23:13:05 +0100

    working statistics, bubble sort added

--- projekt-sortowanie/main.c
+++ projekt-sortowanie/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define TAB_MAX 500
-
+#define TAB_MAX 2000
+#define TIME_MULT 1000000
 //struktura statystyk
 typedef struct
 {
@@ -12,15 +12,16 @@ typedef struct
     int quick;
     int merge;
     //statystyki czasowe
-    int bubble_t;
-    int heap_t;
-    int quick_t;
-    int merge_t;
+    float bubble_t;
+    float heap_t;
+    float quick_t;
+    float merge_t;
     
 }statistics;
 
 //predefinicje funkcji
 void fillTab(int tab[]);
+void deleteStats(statistics *s);
 //funkcje sortowania zwracaja ilosc porownan
 void bubbleSort(int tab[],statistics *s);
 void heapSort(int tab[],statistics *s); //pamietac o ->
@@ -28,13 +29,27 @@ void quickSort(int tab[],statistics *s);
 void mergeSort(int tab[],statistics *s);
 
 
-void getStatistics(statistics s);
+void getStatistics(statistics *s);
 
 int main() {
     int tab[TAB_MAX];
     fillTab(tab);
-    statistics stat;
-    //bubbleSort(tab,stat);
+    statistics s = {
+        .bubble= 0,
+        .bubble_t =  0.0,
+        .heap =  0,
+        .heap_t =  0.0,
+        .quick =  0,
+        .quick_t =  0.0,
+        .merge =  0,
+        .merge_t =  0.0
+    };
+    statistics *stat = &s;
+    
+    //deleteStats(stat);
+    bubbleSort(tab,stat);
+    
+    getStatistics(stat);
     
 }
 
@@ -42,25 +57,66 @@ void fillTab(int tab[])
 {
     srand(time(NULL));
     for(int i=0; i<TAB_MAX; i++)
-        tab[i] = (rand() % 1201) - 600; //losuje liczby z przedzialu <-600;600>
+        tab[i] = (rand() % (4*TAB_MAX+1)) - 2*TAB_MAX; //losuje liczby z przedzialu <-2TAB_MAX;2TAB_MAX>
     return;
 }
-
-void getStatistics(statistics s)
+void deleteStats(statistics *s)
+{
+    s->bubble = 0;
+    s->bubble_t = 0.0;
+    s->heap = 0;
+    s->heap_t = 0.0;
+    s->merge = 0;
+    s->merge_t = 0.0;
+    s->quick = 0;
+    s->quick_t = 0.0;
+    
+}
+void getStatistics(statistics *s)
 {
     printf("\nLiczba porownan:");
-    printf("\nSortowanie babelkowe:\t%d",s.bubble);
-    printf("\nSortowanie przez kopcowanie:\t%d",s.heap);
-    printf("\nSortowanie szybkie:\t%d",s.quick);
-    printf("\nSortowanie przez scalanie:\t%d",s.merge);
+    printf("\nSortowanie babelkowe:\t%d",s->bubble);
+    printf("\nSortowanie przez kopcowanie:\t%d",s->heap);
+    printf("\nSortowanie szybkie:\t%d",s->quick);
+    printf("\nSortowanie przez scalanie:\t%d",s->merge);
     
     printf("\n\nCzas wykonywania:");
-    printf("\nSortowanie babelkowe:\t%d",s.bubble_t);
-    printf("\nSortowanie przez kopcowanie:\t%d",s.heap_t);
-    printf("\nSortowanie szybkie:\t%d",s.quick_t);
-    printf("\nSortowanie przez scalanie:\t%d",s.merge_t);
+    printf("\nSortowanie babelkowe:\t%.2fus",s->bubble_t);
+    printf("\nSortowanie przez kopcowanie:\t%.2fus",s->heap_t);
+    printf("\nSortowanie szybkie:\t%.2fus",s->quick_t);
+    printf("\nSortowanie przez scalanie:\t%.2fus\n\n",s->merge_t);
     
     return;
 }
 
-bubbleSort(int tab[],statistics *s);
+void bubbleSort(int tab[],statistics *s)
+{
+    int max_idx; //indeks najwiekszego elementu
+    int comp = 0; //zlicza porownania
+    clock_t start, stop;
+    float time;
+    
+    start = clock();
+    for(int i=0; i < TAB_MAX; i++)
+    {
+        max_idx = 0;
+        for(int j=0; j< TAB_MAX - i; j++)
+        {
+            if (tab[max_idx]<tab[j])
+                max_idx = j;
+            comp++; //porownuje elementy
+            
+        }
+        //zamiana wartosci
+        int z = tab[max_idx];
+        tab[max_idx] = tab[TAB_MAX-i-1];
+        tab[TAB_MAX-i-1] = z;
+    }
+    stop = clock();
+    time = TIME_MULT*(stop-start)/CLOCKS_PER_SEC;
+    
+    s->bubble = comp;
+    s->bubble_t = time;
+    
+    return;
+}
