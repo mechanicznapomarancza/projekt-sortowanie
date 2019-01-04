813478af3f90ccc65c166eaf8a14047c34d66e6e
Krzysztof Zawadka <kzawadka@macbook-pro.local>
2019-01-04 21:53:53 +0100

    Initial Commit

---
+++ projekt-sortowanie/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  projekt-sortowanie
+//
+//  Created by Krzysztof Zawadka on 04/01/2019.
+//  Copyright © 2019 Krzysztof Zawadka. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
